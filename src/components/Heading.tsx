10015cb51b4d77f2b9123e77c173859c94b40c70
Nico Hermida <hermida.nicolas101@gmail.com>
2024-06-12 17:14:01 -0300

    add brand in heading component

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,8 +1,12 @@
+import { AppLogo } from './AppLogo'
+
 export function Heading() {
 	return (
-		<div className='w-full h-96 relative flex justify-center items-center'>
+		<div className='w-full h-96 relative flex flex-col justify-center items-center'>
 			<div className='w-[1000px] h-[600px] absolute -z-10 heading-gradient'></div>
-			<h1 className='text-3xl font-bold w-[600px] text-center'>
+			<AppLogo height={130} width={500} />
+
+			<h1 className='text-2xl font-bold text-center mt-6'>
 				Conocimientos para programadores en cada click.
 			</h1>
 		</div>
