86f03def2838a279ec07452dd3113a84f17cc90a
Nico Hermida <hermida.nicolas101@gmail.com>
2024-06-14 23:01:09 -0300

    add link in topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,12 +2,15 @@ import { AppLogo } from './AppLogo'
 import { LoginButton } from './LoginButton'
 import { RegisterButton } from './RegisterButton'
 import { Searchbox } from './Searchbox'
+import Link from 'next/link'
 
 export function Topbar() {
 	return (
 		<header className='w-full bg-white py-2 shadow-sm fixed z-10'>
 			<div className='w-full max-w-app mx-auto flex justify-between px-10'>
-				<AppLogo />
+				<Link href='/'>
+					<AppLogo />
+				</Link>
 
 				<Searchbox />
 
